5f8313c72dca2fb932b5dc6925c4d20342f29941
Sam <sam.morgan44@gmail.com>
2020-02-08 12:20:15 -0600

    testing automatic build

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,11 @@
 public class HelloWorld{
     public static void main(String[] args){
         System.out.println("Hello World");
+        System.out.println(sum(1,2));
     }
 
     //Can use this function to test our tests
-    public int sum(int x, int y){
+    public static int sum(int x, int y){
         return x + y;
     }
 }
