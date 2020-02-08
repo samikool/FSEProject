63ad9f20b212177a6a5cf0e9b817fede4eb4e24b
Sam <sam.morgan44@gmail.com>
2020-02-08 11:34:03 -0600

    added hellworld test program

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+
+    //Can use this function to test our tests
+    public int sum(int x, int y){
+        return x + y;
+    }
+}
