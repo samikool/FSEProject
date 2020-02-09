2ea72792b84509ca81b0f7266b72a5dcb0e55719
Sam <sam.morgan44@gmail.com>
2020-02-08 18:13:56 -0600

    testing automatic testing

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -5,6 +5,6 @@ import static org.junit.Assert.assertEquals;
 public class HelloWorldTest{
     @Test 
     public void testAdd(){
-        assertEquals(HelloWorld.sum(1, 2), 3);
+        assertEquals(HelloWorld.sum(4, 2), 6);
     }
 }
