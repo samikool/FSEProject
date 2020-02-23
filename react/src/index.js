49ed672536e69a4e252856eebf232149dfe03302
Sam <sam.morgan44@gmail.com>
2020-02-23 16:26:54 -0600

    Initial commit of simple react app

---
+++ react/src/index.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
+
+class Clock extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {date: new Date()}
+  }
+
+  componentDidMount(){
+    this.timerID = setInterval(
+      () => this.tick(), 
+      1000
+      )
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.timerID);
+  }
+
+  tick(){
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render(){
+    return(
+      <h4>{this.state.date.toLocaleTimeString()}</h4>
+    );
+  }
+}
+
+class Login extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {username: '', password: ''}
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event){
+    const{name, value} = event.target;
+    this.setState({[name]: value});
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+    console.log("here");
+  }
+
+  render(){
+    return(
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="uname">Username:</label>
+          <input 
+            type="text" 
+            id="uname" 
+            value={this.state.username} 
+            name="username" 
+            onChange={this.handleChange}/>
+          <br />
+          <label htmlFor="pass">Password:</label>
+          <input 
+            type="password" 
+            id="pass" 
+            value={this.state.password} 
+            name="password"
+            onChange={this.handleChange}/>
+          <br />
+          <input
+            type="submit" 
+            value="Submit">
+          </input>
+        </form>
+      </div>
+    );
+  }
+}
+
+
+
+ReactDOM.render(
+  <div>
+    <h4>Welcome</h4>
+    <Clock/>
+    <Login/>
+  </div>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
