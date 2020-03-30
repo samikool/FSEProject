a499a67b7c6eea7964468f56ce41786151849f9f
Antonio Washington <senortonito@gmail.com>
2020-03-30 10:15:03 -0500

    populate db file working

--- backend_js/db_management/populateDb.js
+++ backend_js/db_management/populateDb.js
@@ -1,5 +1,5 @@
 const { Pool, Client } = require('pg')
-const bcrypt = require('bcrypt')
+
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
@@ -8,66 +8,65 @@ const pool = new Pool({
   port: 5432,
 })
 
+const dbApi = require('./dbApi')
+const db = new dbApi(pool)
+let test_user;
 
 
-const array = [
-    {"First_Name":"Antonio","Last_Name":"Washington","Password":"password","Email":"senortonito@gmail.com", "Location":{"Address":"712 Pawnee Ln", "Country":"United States", "State":"Illinois", "City":"Naperville", "Zipcode":60563}, "isadmin":true},
-    {"First_Name":"Testing","Last_Name":"Guy","Password":"password", "Email":"testing.guy@tmail.com", "Location":{"Address":"123 Example", "Country":"United States", "State":"Florida", "City":"Orlando", "Zipcode":32862}, "isadmin":false},
-    {"First_Name":"Babu","Last_Name":"Slapps","Password":"password", "Email":"babu.slapps@tmail.com", "Location":{"Address":"111 Illiois Rd", "Country":"United States", "State":"Illinois", "City":"Naperville", "Zipcode":60563}, "isadmin":false},
-    {"First_Name":"Gordon","Last_Name":"Ramstein","Password":"password", "Email":"gordon.gluten@tmail.com", "Location":{"Address":"001 Big Road Ln", "Country":"United States", "State":"Illinois", "City":"Naperville", "Zipcode":60560}, "isadmin":false},
-];
-var query_str = `INSERT INTO Users (First_Name, Last_Name, Email, Password, Location, isadmin)
-VALUES( '${array[0].First_Name}',
-        '${array[0].Last_Name}',
-        '${array[0].Email}',
-        '${array[0].Password}',
-        '${JSON.stringify(array[0].Location)}',
-        '${array[0].isadmin}');`;
-
-console.log(query_str);
-pool
-    .query(query_str)
-    .then(res => {
-        pool.query("SELECT * FROM USERS;")
-            .then(res_2 => console.log(res_2.rows))
-    })
-    .catch(e => {
-        console.error(e.detail)
-        pool.query("SELECT * FROM USERS;")
-            .then(res_2 => console.log(res_2.rows))
-    })
-
-for (let index = 0; index < array.length; index++) {
+/**
+ * Test & Initialization function to fill the Disaster Table
+ * @param {*} array 
+ */
+async function FillDisasters(array){
+  for (let index = 0; index < array.length; index++) {
     const element = array[index];
-    // query_str = `INSERT INTO "Users" ("First_Name", "Last_Name", "Email", "Password", "Location", "isadmin")VALUES('${element.First_Name}',
-    //                     '${element.Last_Name}',
-    //                     '${element.Email}',
-    //                     '${element.Password}',
-    //                     '${element.Location}',
-    //                     '${element.isadmin}');`;
-    // pool
-    //     .query(query_str)
-    //     .then(res => console.log("Pushed Data!", res))
-    //     .catch(e => console.log(e))
-    element.password = bcrypt.hash(element.password,10)
-    var query_str = `INSERT INTO Users (First_Name, Last_Name, Email, Password, Location, isadmin)
-    VALUES( '${element.First_Name}',
-            '${element.Last_Name}',
-            '${element.Email}',
-            '${element.Password}',
-            '${JSON.stringify(element.Location)}',
-            '${element.isadmin}');`;
-    
-    console.log(query_str);
-    pool
-        .query(query_str)
-        .then(res => {
-            pool.query("SELECT * FROM USERS;")
-                .then(res_2 => console.log(res_2.rows))
-        })
-        .catch(e => {
-            console.error(e.detail)
-            pool.query("SELECT * FROM USERS;")
-                .then(res_2 => console.log(res_2.rows))
-        })
+    db.AddDisaster(element.name,element.location,element.keywords)
+  }
 }
+
+/**
+ * Test & Initialization function to fill the Items Table
+ * @param {*} array 
+ */
+async function FillItems(array){
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    db.AddItem(element.name,element.type,element.keywords)  
+  }
+}
+
+/**
+ * Test & Initialization function to fill the Users Table
+ * @param {*} array 
+ */
+async function FillUsers(array){
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    db.NewUser(element.first_name,element.last_name,element.password,element.email,element.location,element.isadmin,element.isDonor,element.isRequester)
+  }
+}
+
+const user_array = [
+  {"first_name":"Antonio","last_name":"Washington","password":"password","email":"senortonito@gmail.com", "location":{"Address":"712 Pawnee Ln", "Country":"United States", "State":"Illinois", "City":"Naperville", "Zipcode":60563}, "isadmin":true,"isDonor":true, "isRequester":true},
+  {"first_name":"Testing","last_name":"Guy","password":"password", "email":"testing.guy@tmail.com", "location":{"Address":"123 Example", "Country":"United States", "State":"Florida", "City":"Orlando", "Zipcode":32862}, "isadmin":false,"isDonor":true, "isRequester":true},
+  {"first_name":"Babu","last_name":"Slapps","password":"password", "email":"babu.slapps@tmail.com", "location":{"Address":"111 Illiois Rd", "Country":"United States", "State":"Illinois", "City":"Naperville", "Zipcode":60563}, "isadmin":false,"isDonor":true, "isRequester":true},
+  {"first_name":"Gordon","last_name":"Ramstein","password":"password", "email":"gordon.gluten@tmail.com", "location":{"Address":"001 Big Road Ln", "Country":"United States", "State":"Illinois", "City":"Naperville", "Zipcode":60560}, "isadmin":false,"isDonor":true, "isRequester":true},
+];
+FillUsers(user_array);
+
+const dis_array = [
+  {"name":"Katrina","location":{"country":"USA","state":"Texas","city":"Dallas"},"keywords":{"type":["fire"],"items_need":["towels","food"]}},
+  {"name":"Brookfield","location":{"country":"USA","state":"Illinois","city":"Chicago"},"keywords":{"type":["fire"],"items_need":["food"]}},
+  {"name":"San Jose","location":{"country":"USA","state":"California","city":"San Jose"},"keywords":{"type":["earthquake"],"items_need":["construction"]}},
+  {"name":"Jenny","location":{"country":"USA","state":"Iowa","city":"Iowa City"},"keywords":{"type":["tornado"],"items_need":["food"]}}
+]
+FillDisasters(dis_array);
+
+const item_array = [
+  {"name":"paper towels","type":"cleaning","keywords":["large","paper"]},
+  {"name":"soap","type":"cleaning","keywords":["small","bar"]},
+  {"name":"hand sanitizer","type":"cleaning","keywords":["bacterial","travel"]},
+  {"name":"canned soup","type":"food","keywords":["soup","vegetable"]},
+  {"name":"vegetables","type":"food","keywords":["carrot","broccoli"]}
+]
+FillItems(item_array);
