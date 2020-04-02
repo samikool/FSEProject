187c91997188b4a6d847fa6b09489a9667beaff1
Antonio Washington <senortonito@gmail.com>
2020-04-02 00:24:02 -0500

    Marker work for frontend

--- frontend_react/src/disasterMarker.jsx
+++ frontend_react/src/disasterMarker.jsx
@@ -1,11 +1,29 @@
+//resources: https://react.semantic-ui.com/modules/modal
 import React, { Component } from 'react';
-import Button from '@material-ui/core/Button'
+// import Button from '@material-ui/core/Button';
+import PropTypes from 'prop-types';
+// import { Button } from 'react-bootstrap';
+import GoogleMapReact from 'google-map-react';
+import { Button, Header, Image, Icon, Modal } from 'semantic-ui-react'
+import 'semantic-ui-less/semantic.less'
+import 'semantic-ui-css/semantic.min.css';
 
-export default class DisasterMarker extends React.Component{
+
+const pinStyle={
+    borderRadius: '10px',
+    transform: 'matrix(-1, 0, 0, 1, 10, 0)'
+  }
+
+
+export default class DisasterMarker extends Component{
     constructor(props){
         super(props);
+        this.state = {"isShown":false, "setIsShown":false};
     }
-
+    
+    async onLoad(){
+        console.log("loaded")
+    }
     async handleClick(){
         console.log('here');
     }
@@ -13,8 +31,26 @@ export default class DisasterMarker extends React.Component{
     render(){
         return(
             <div>
-                <Button onClick = {this.handleClick} color="inherit">Marker</Button>
+            <Modal size={'mini'} trigger={
+                <Icon name="fire" 
+                size={'big'} 
+                style={pinStyle} 
+                onClick={this.handleClick}
+                />}>
+                    <Modal.Header>Disaster</Modal.Header>
+                    <Modal.Content image>
+                        <Icon name="fire" size='massive'></Icon>
+                        <Modal.Description>
+                        <Header>Disaster Properties</Header>
+                            <p>Name: Example Name</p>
+                            <p>Desciption: Brief description of the disaster</p>
+                            <p>Type: Example Type (ie. Fire)</p>
+                            <p>Location: Example Location (ie. Chicago, IL)</p>
+                            <p>Keywords: Example Keywords</p>
+                        </Modal.Description>
+                    </Modal.Content>
+                </Modal>
             </div>
         );
     }
-}
+} 
--- frontend_react/src/map.js
+++ frontend_react/src/map.js
@@ -1,13 +1,50 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 import DisasterMarker from './disasterMarker';
+import PropTypes from 'prop-types';
+import { Marker, GoogleMap, LoadScript  } from '@react-google-maps/api';
 
+const onLoad = marker => {
+  console.log('marker: ', marker);
+}
+
+const position = {
+  lat:0.955413,
+  lng:0.337844
+}
+const mapContainerStyle = {
+  height: "400px",
+  width: "800px"
+}
+
+const center = {
+  lat: 0,
+  lng: -180
+}
 
 
 class SimpleMap extends Component {
   constructor(props){
     super(props);
     console.log(process.env)
+  }
+
+  // Store the value of the current disaster?
+  // this can possibly handled in the marker class
+  state = {
+    disasterName: "",
+    lat: "",
+    lng: "",
+    center: "",
+    zoom: this.props.zoom,
+    hover: false,
+    currentPosition: false,
+    infoBox: false
+  }
+
+  static defaultProps = {
+    center: {lat: 40.73, lng: -73.93}, 
+    zoom: 12
   }
 
   render() {
@@ -18,9 +55,11 @@ class SimpleMap extends Component {
           bootstrapURLKeys={{key: process.env.REACT_APP_GOOGLE_API_KEY}}
           defaultCenter={{lat: 20, lng: 0}}
           defaultZoom={1}
+          onChildMouseEnter={this.onChildMouseEnter}
+          onChildMouseLeave={this.onChildMouseLeave}
           yesIWantToUseGoogleMapApiInternals
         >
-          <DisasterMarker
+        <DisasterMarker
             lat={0.955413}
             lng={0.337844}
           />
